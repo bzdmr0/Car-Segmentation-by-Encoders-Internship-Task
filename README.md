a90808db56f27cd264e63e583021cbfa31499aa8
Your name <your.email@example>
2025-09-12 18:01:10 +0300

    last_update

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # Car-Segmentation-by-Encoders-Internship-Task
 Vehicle segmentation using the segmentation-models-pytorch library and comparing the results with different encoders.
+
+## Dependencies
+This project requires the following Python packages (installed in a virtual environment):
+
+- certifi==2025.8.3
+- charset-normalizer==3.4.3
+- colorama==0.4.6
+- filelock==3.19.1
+- fsspec==2025.9.0
+- huggingface-hub==0.34.4
+- idna==3.10
+- Jinja2==3.1.6
+- MarkupSafe==3.0.2
+- mpmath==1.3.0
+- networkx==3.4.2
+- numpy==2.2.6
+- packaging==25.0
+- pillow==11.3.0
+- PyYAML==6.0.2
+- requests==2.32.5
+- safetensors==0.6.2
+- segmentation_models_pytorch==0.5.0
+- sympy==1.13.1
+- timm==1.0.19
+- torch==2.5.1+cu121
+- torchaudio==2.5.1+cu121
+- torchvision==0.20.1+cu121
+- tqdm==4.67.1
+- typing_extensions==4.15.0
+- urllib3==2.5.0
+
+To install these dependencies, create a virtual environment and run:
+```bash
+pip install -r requirements.txt
+```
+
+Note: The PyTorch packages (torch, torchvision, torchaudio) are installed with CUDA 12.1 support for GPU acceleration.
+
+
+## Loading data
+For this example we will use CamVid dataset. It is a set of:
+
+- train images + segmentation masks
+- validation images + segmentation masks
+- test images + segmentation masks
+
+All images have 320 pixels height and 480 pixels width. For more inforamtion about dataset visit 
+http://mi.eng.cam.ac.uk/research/projects/VideoRec/CamVid/.
+
+# To download Data
+```bash
+git clone https://github.com/alexgkendall/SegNet-Tutorial ./data
+```
---
+++ main.py
@@ -0,0 +1,162 @@
+import os
+import cv2
+
+import torch
+import numpy as np
+import albumentations as A
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset as BaseDataset
+from torch.optim import lr_scheduler
+import segmentation_models_pytorch as smp
+import pytorch_lightning as pl
+
+DATA_DIR = "./data/CamVid/"
+
+x_train_dir = os.path.join(DATA_DIR, "train")
+y_train_dir = os.path.join(DATA_DIR, "trainannot")
+
+x_valid_dir = os.path.join(DATA_DIR, "val")
+y_valid_dir = os.path.join(DATA_DIR, "valannot")
+
+x_test_dir = os.path.join(DATA_DIR, "test")
+y_test_dir = os.path.join(DATA_DIR, "testannot")
+
+class Dataset(BaseDataset):
+    """CamVid Dataset. Read images, apply augmentation transformations.
+
+    Args:
+        images_dir (str): path to images folder
+        masks_dir (str): path to segmentation masks folder
+        class_values (list): values of classes to extract from segmentation mask
+        augmentation (albumentations.Compose): data transfromation pipeline
+            (e.g. flip, scale, etc.)
+
+    """
+
+    CLASSES = [
+        "sky",
+        "building",
+        "pole",
+        "road",
+        "pavement",
+        "tree",
+        "signsymbol",
+        "fence",
+        "car",
+        "pedestrian",
+        "bicyclist",
+        "unlabelled",
+    ]
+
+    def __init__(
+        self,
+        images_dir,
+        masks_dir,
+        classes=None,
+        augmentation=None,
+    ):
+        self.ids = os.listdir(images_dir)
+        self.images_fps = [os.path.join(images_dir, image_id) for image_id in self.ids]
+        self.masks_fps = [os.path.join(masks_dir, image_id) for image_id in self.ids]
+
+        # convert str names to class values on masks
+        self.class_values = [self.CLASSES.index(cls.lower()) for cls in classes]
+
+        self.augmentation = augmentation
+
+    def __getitem__(self, i):
+        image = cv2.imread(self.images_fps[i])
+        # BGR-->RGB
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(self.masks_fps[i], cv2.IMREAD_GRAYSCALE)
+
+        # extract certain classes from mask (e.g. cars)
+        masks = [(mask == v) for v in self.class_values]
+        mask = np.stack(masks, axis=-1).astype("float")
+
+        # apply augmentations
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample["image"], sample["mask"]
+
+        return image.transpose(2, 0, 1), mask.transpose(2, 0, 1)
+
+    def __len__(self):
+        return len(self.ids)
+    
+# helper function for data visualization
+def visualize(**images):
+    """PLot images in one row."""
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(" ".join(name.split("_")).title())
+        if name == "image":
+            plt.imshow(image.transpose(1, 2, 0))
+        else:
+            plt.imshow(image)
+    plt.show()
+    
+# training set images augmentation
+def get_training_augmentation():
+    train_transform = [
+        A.HorizontalFlip(p=0.5),
+        A.ShiftScaleRotate(
+            scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0
+        ),
+        A.PadIfNeeded(min_height=320, min_width=320, always_apply=True),
+        A.RandomCrop(height=320, width=320, always_apply=True),
+        A.GaussNoise(p=0.2),
+        A.Perspective(p=0.5),
+        A.OneOf(
+            [
+                A.CLAHE(p=1),
+                A.RandomBrightnessContrast(p=1),
+                A.RandomGamma(p=1),
+            ],
+            p=0.9,
+        ),
+        A.OneOf(
+            [
+                A.Sharpen(p=1),
+                A.Blur(blur_limit=3, p=1),
+                A.MotionBlur(blur_limit=3, p=1),
+            ],
+            p=0.9,
+        ),
+        A.OneOf(
+            [
+                A.RandomBrightnessContrast(p=1),
+                A.HueSaturationValue(p=1),
+            ],
+            p=0.9,
+        ),
+    ]
+    return A.Compose(train_transform)
+
+
+def get_validation_augmentation():
+    """Add paddings to make image shape divisible by 32"""
+    test_transform = [
+        A.PadIfNeeded(384, 480),
+    ]
+    return A.Compose(test_transform)
+    
+dataset = Dataset(x_train_dir, y_train_dir, classes=["car"])
+
+# Visualize resulted augmented images and masks
+augmented_dataset = Dataset(
+    x_train_dir,
+    y_train_dir,
+    augmentation=get_training_augmentation(),
+    classes=["car"],
+)
+
+# same image with different random transforms
+for i in range(3):
+    image, mask = augmented_dataset[3]
+    visualize(image=image, mask=mask.squeeze())
